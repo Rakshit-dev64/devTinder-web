a731755197d14477310a308160db602f4280293e
Rakshit-dev64 <rakshityagi2103@gmail.com>
2025-06-17 01:18:05 +0530

    Routing

---
+++ src/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './navBar';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
+
+const Body = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
