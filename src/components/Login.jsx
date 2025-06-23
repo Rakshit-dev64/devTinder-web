6a684325528f072cb6354d0803a761e2d1380dec
Rakshit-dev64 <rakshityagi2103@gmail.com>
2025-06-23 23:22:50 +0530

    set up Redux store and created login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { InputField, PassWordField } from './components'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import BASE_URL from '../utils/constants';
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("rakshit@gmail.com");
+  const [password, setPassword] = useState("Rakshit@998")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+      try{
+      const res = await axios.post(BASE_URL + "/login", {emailId, password},
+      {withCredentials : true}
+      )
+      dispatch(addUser(res.data));
+      return navigate("/");
+  }
+  catch(err){
+    console.error(err);
+    }
+}
+
+  return (
+   <div className='flex justify-center mt-32 card-xl'>
+    <div className="card bg-neutral w-96 shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title justify-center mb-5">Login</h2>
+    <InputField value={emailId} onChange={(e)=>{setEmailId(e.target.value)}}/>
+    <PassWordField value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+    <div className="card-actions justify-center mt-4">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
+   </div>
+  )
+}
+
+export default Login
---
+++ src/components/components.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+
+export const NavBar = () => {
+  const user = useSelector((store)=>store.user);
+  return (
+      <div className="navbar bg-neutral shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl bg-neutral-content text-neutral ml-4">DevTinder</a>
+  </div>
+  <div className="flex gap-2">
+    {user && (<h1 className='mt-2'>Welcome {user.firstName}</h1>)}
+    <div className="dropdown dropdown-end mx-6">
+      {user && (<div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src= {user.profileURL} />
+        </div>
+      </div>)}
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export const InputField = ({value, onChange}) => {
+  return(
+    <>
+    <label className="input validator mb-7">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+      <circle cx="12" cy="7" r="4"></circle>
+    </g>
+  </svg>
+  <input
+    type="email" 
+    placeholder="Email ID" 
+    required
+    value={value}
+    onChange={onChange}
+    />
+</label>
+</>
+  )
+}
+
+export const PassWordField = ({value, onChange}) => {
+  return(
+    <>
+    <label className="input validator">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <path
+        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+      ></path>
+      <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+    </g>
+  </svg>
+  <input
+    type="password"
+    required
+    placeholder="Password"
+    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+    title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+    value={value}
+    onChange={onChange}
+  />
+</label>
+    </>
+  )
+}
